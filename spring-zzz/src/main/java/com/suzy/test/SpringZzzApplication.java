a022b21afc20acb0a7f3b79e4048757e40236157
Suzy <suzhongyuan55@163.com>
2020-11-16 21:33:01 +0800

    init

---
+++ spring-zzz/src/main/java/com/suzy/test/SpringZzzApplication.java
@@ -0,0 +1,13 @@
+package com.suzy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZzzApplication.class, args);
+	}
+
+}
